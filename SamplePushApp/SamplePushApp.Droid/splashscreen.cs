c0df8c8bc56d292821361b265796d6ea6117550e
mcsako <ankitha.kotha@mcssolutions.com>
2019-08-07 12:41:58 +0530

    change splash screen

---
+++ SamplePushApp/SamplePushApp.Droid/splashscreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SamplePushApp.Droid
+{
+    [Activity(Label = "Splash Screen", MainLauncher = true, Theme = "@style/Theme.Splash", NoHistory = true)]
+    class splashscreen:Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //Display Splash Screen for 4 Sec  
+            Thread.Sleep(4000);
+            //Start Activity1 Activity  
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
